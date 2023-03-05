43d4ae20a36c7653ac809570090af0de55a5272a
Ventura Lucena Martínez <44477740+VenturaLM@users.noreply.github.com>
2023-03-05 18:18:22 +0100

    Repo initialized.

---
+++ utils/merkletree.js
@@ -0,0 +1,14 @@
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+
+let whiteList = [
+    "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
+    "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2",
+    "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
+    "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"
+];
+
+const leafNodes = whiteList.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
+console.log("WhitelistA Merkle Tree:\n", merkleTree.toString());
